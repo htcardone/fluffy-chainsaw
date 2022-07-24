8153a5eaad1b3697d6c0c3aa94a1eee90962ac75
Henrique Cardone <htcardone@gmail.com>
2022-07-24 08:09:15 -0300

    Fix crash when a network exception is thrown

--- app/src/main/java/com/podium/technicalchallenge/data/sources/genres/remote/GenresRemoteDataSource.kt
+++ app/src/main/java/com/podium/technicalchallenge/data/sources/genres/remote/GenresRemoteDataSource.kt
@@ -1,12 +1,11 @@
 package com.podium.technicalchallenge.data.sources.genres.remote
 
-import com.google.gson.Gson
 import com.podium.technicalchallenge.data.Result
 import com.podium.technicalchallenge.data.entity.GenresResponse
 import com.podium.technicalchallenge.data.network.queries.Queries
 import com.podium.technicalchallenge.data.network.retrofit.GraphQLService
+import com.podium.technicalchallenge.data.network.retrofit.GraphQlHelper
 import com.podium.technicalchallenge.data.sources.genres.GenresDataSource
-import org.json.JSONObject
 
 class GenresRemoteDataSource(
     private val service: GraphQLService
@@ -15,15 +14,8 @@ class GenresRemoteDataSource(
     // TODO Add a trace to measure how long this request is taking
     override suspend fun getAllGenres(): Result<List<String>> {
 
-        val params = JSONObject().apply {
-            put("query", Queries.getGenres())
-        }
-
-        val response = service.postGraphQL(params.toString())
-        val jsonBody = response.body()
-
         return try {
-            val data = Gson().fromJson(jsonBody, GenresResponse::class.java)
+            val data = GraphQlHelper.post(Queries.getGenres(), service, GenresResponse::class.java)
 
             if (data != null) {
                 Result.Success(data.data.genres)

d17cc9d9a396ea33e2cbdd946d620047fdc9b7fd
Henrique Cardone <htcardone@gmail.com>
2022-07-24 21:48:44 -0300

    Enable StrictMode for debug builds

--- app/src/main/java/com/podium/technicalchallenge/MoviesApp.kt
+++ app/src/main/java/com/podium/technicalchallenge/MoviesApp.kt
@@ -1,10 +1,29 @@
 package com.podium.technicalchallenge
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MoviesApp: Application() {
 
-    // TODO add StrictMode
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .build())
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .build())
+        }
+    }
 }
